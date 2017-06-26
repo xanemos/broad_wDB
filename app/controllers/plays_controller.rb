b792ff15811e8b5b592b29cd242d0b5f0e435480
root <root@localhost.localdomain>
2017-06-26 21:46:59 +0300

    Category Model and assigned Plays to Users

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -10,11 +10,11 @@ class PlaysController < ApplicationController
 	end
 
 	def new
-		@play = current_user.play.bilt
+		@play = current_user.plays.build
 	end
 
 	def create
-		@play = current_user.play.bilt(play_params)
+		@play = current_user.play.build(play_params)
 		if @play.save
 			redirect_to root_path
 		else
